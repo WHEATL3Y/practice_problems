cd40ff1283307f702a28baafea536173a1c3dd44
Jacob Christensen <jchristensen888@gmail.com>
2023-06-04 16:31:12 -0600

    change long long to long since they're the same size

--- fibonacci/c/iterFib.c
+++ fibonacci/c/iterFib.c
@@ -6,25 +6,25 @@
 
 #define LIMIT 100 
 
-int willOverflow(unsigned long long n1, unsigned long long n2) {
+int willOverflow(unsigned long n1, unsigned long n2) {
 
-    return (n2 > ULLONG_MAX - n1);
+    return (n2 > ULONG_MAX - n1);
 
 }
 
 unsigned int fib() {
 
-    unsigned long long n1 = 0;
-    unsigned long long n2 = 1;
-    unsigned long long n3;
+    unsigned long n1 = 0;
+    unsigned long n2 = 1;
+    unsigned long n3;
 
-    printf("%llu\n%llu\n", n1, n2);
+    printf("%lu\n%lu\n", n1, n2);
 
     while (!willOverflow(n1, n2)) {
         n3 = n1 + n2;
         n1 = n2;
         n2 = n3;
-        printf("%llu\n", n3);
+        printf("%lu\n", n3);
     }
 
     return n3;
@@ -34,6 +34,5 @@ unsigned int fib() {
 int main(void) {
     
     fib();
-    printf("Long: %ul\nLong Long: %ull\n", ULONG_MAX, ULLONG_MAX);
     return 0;
 }
